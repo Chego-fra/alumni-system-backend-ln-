c4b417cafd032da399644ae7b2a99665e08fabf3
Francis Chege <chegefrancis664@gmail.com>
2025-03-20 12:54:17 +0300

    created career reply controler

--- app/Http/Controllers/API/CareerReplyController.php
+++ app/Http/Controllers/API/CareerReplyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\CareerReply;
 use Illuminate\Http\Request;
 
 class CareerReplyController extends Controller
@@ -10,9 +11,15 @@ class CareerReplyController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $query = CareerReply::query();
+
+        if ($request->has('search')) {
+            $query->where('message', 'LIKE', "%{$request->search}%");
+        }
+
+        return response()->json($query->paginate(10));
     }
 
     /**
@@ -20,15 +27,23 @@ class CareerReplyController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'career_id' => 'required|exists:careers,id',
+            'alumni_id' => 'required|exists:alumni_profiles,id',
+            'message' => 'required|string',
+        ]);
+
+        $reply = CareerReply::create($data);
+
+        return response()->json($reply, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(CareerReply $reply)
     {
-        //
+        return response()->json($reply);
     }
 
     /**
@@ -36,14 +51,22 @@ class CareerReplyController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $data = $request->validate([
+            'message' => 'required|string',
+        ]);
+    
+        $reply = CareerReply::findOrFail($id);
+        $reply->update($data);
+    
+        return response()->json($reply);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(CareerReply $reply)
     {
-        //
+        $reply->delete();
+        return response()->json(['message' => 'Deleted successfully']);
     }
 }
